6e6c612c2fd5fd1ea1512d15d3dfc08610ccab25
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-04-13 11:30:20 -0400

    Variable struct
    Implemented bare-bones Variable type structure.

--- src/GDB.jl
+++ src/GDB.jl
@@ -1,6 +1,10 @@
 module GDB
 
-export test_simulation
+export test_simulation, Variable, Density, Temp
+
+using Distributed
+@everywhere using LinearAlgebra
+@everywhere include("./variables.jl")
 
 # have placeholder test_simulation for now
 function test_simulation()
