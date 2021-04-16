802e43703e5bbeead7668864117dc1bbc7ada6d8
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-04-15 22:27:41 -0400

    p_jacobi
    actually functioning parallel jacobi iterator!

--- src/GDB.jl
+++ src/GDB.jl
@@ -1,13 +1,15 @@
 module GDB
 
 export test_simulation, Variable, Density, Temp
-export jacobi, laplacian2d
+export jacobi, laplacian2d, dist_jacobi
 
 using Distributed
 @everywhere using LinearAlgebra
+@everywhere using SparseArrays
 @everywhere include("./variables.jl")
 @everywhere include("./jacobi.jl")
 @everywhere include("./laplacian2d.jl")
+@everywhere include("./p_jacobi.jl")
 
 # have placeholder test_simulation for now
 function test_simulation()
