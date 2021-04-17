ccfaff83150960a63f2be20596fbf8a58d59b13e
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-04-17 01:42:07 -0400

    hilbert.jl
    moved hilbert ordering function into its own file. I don't expect this to get any use.

--- src/prim/grids.jl
+++ src/prim/grids.jl
@@ -1,39 +1,5 @@
 
 
-# NOTE: This does what its supposed to do but I don't think its
-# a good idea to use it actually.
-# NOTE: I just copied this from wikipedia, not sure how it actually works
-function hilbert_curve(d::Int, n::Int)
-
-    t = d
-    x = 0.0
-    y = 0.0
-
-    for i=1:n
-        s = 2^(i-1)
-        rx = 1 & div(t, 2)
-        ry = 1 & xor(t, rx)
-
-        if (ry == 0)
-            if (rx == 1)
-                x = s - 1 - x
-                y = s - 1 - y
-            end
-
-            r = Int[y, x]
-            x = r[1]
-            y = r[2]
-        end
-
-        x += s * rx
-        y += s * ry
-        t = div(t, 4)
-    end
-
-    return Int[x, y]
-end
-
-
 struct Grid
 
     points::Matrix{Float64}
