249247ea33832c410a29e530583c2e1944a82b2f
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-04-18 17:32:30 -0400

    Update penalize.jl
    removed unnecessarily specific methods of penalize

--- src/prim/penalize.jl
+++ src/prim/penalize.jl
@@ -7,27 +7,8 @@ end
 
 
 # penalize operator in favor of another operator
-function penalize(A::SparseMatrixCSC, B::SparseMatrixCSC, P::Vector{Float64})
+function penalize(A::SparseMatrixCSC, B::SparseMatrixCSC, pen::Vector{Float64})
 
+    P = Diagonal(pen)
     return (I - P) * A + P * B
-end
-
-
-# penalize vector in favor of multiple vectors
-function penalize(a::Vector{Float64}, b::Array{Vector{Float64}, 2})
-
-    for i=1:size(b)[2]
-        a = penalize(a, b[:,i]...)
-    end
-    return a
-end
-
-
-# penalize operator in favor of multiple operators
-function penalize(A::SparseMatrixCSC, B::Array{SparseMatrixCSC, 2})
-
-    for i=1:size(B)[2]
-        A = penalize(A, B[:,i]...)
-    end
-    return A
 end
