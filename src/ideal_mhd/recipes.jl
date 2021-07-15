f3ec8579b49feae42ea614bd78e223977f961398
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-07-15 15:25:20 -0400

    Update recipes.jl
    Commented out the plotsetup recipe, since it uses @layout which is a Plots macro.

--- src/ideal_mhd/recipes.jl
+++ src/ideal_mhd/recipes.jl
@@ -25,40 +25,40 @@ end
 end
 
 
-@userplot PlotSetup
-@recipe function f(ps::PlotSetup)
-
-    stp = ps.args[1]
-    x = stp.grd.points[1,:]
-    y = stp.grd.points[2,:]
-    pvec = diag(stp.P1*stp.P2*stp.P3)
-
-    p0 = vec_to_mesh(pvec, stp.grd)
-    p1 = vec_to_mesh(stp.R1*pvec, stp.grd)
-    p2 = vec_to_mesh(stp.R2*pvec, stp.grd)
-    p3 = vec_to_mesh(stp.R3*pvec, stp.grd)
-
-    layout := @layout [b c; d e]
-    seriestype := :heatmap
-    colorbar := false
-
-    @series begin
-        subplot := 1
-        p0
-    end
-
-    @series begin
-        subplot := 2
-        p1
-    end
-
-    @series begin
-        subplot := 3
-        p2
-    end
-
-    @series begin
-        subplot := 4
-        p3
-    end
-end
+# @userplot PlotSetup
+# @recipe function f(ps::PlotSetup)
+#
+#     stp = ps.args[1]
+#     x = stp.grd.points[1,:]
+#     y = stp.grd.points[2,:]
+#     pvec = diag(stp.P1*stp.P2*stp.P3)
+#
+#     p0 = vec_to_mesh(pvec, stp.grd)
+#     p1 = vec_to_mesh(stp.R1*pvec, stp.grd)
+#     p2 = vec_to_mesh(stp.R2*pvec, stp.grd)
+#     p3 = vec_to_mesh(stp.R3*pvec, stp.grd)
+#
+#     layout := @layout [b c; d e]
+#     seriestype := :heatmap
+#     colorbar := false
+#
+#     @series begin
+#         subplot := 1
+#         p0
+#     end
+#
+#     @series begin
+#         subplot := 2
+#         p1
+#     end
+#
+#     @series begin
+#         subplot := 3
+#         p2
+#     end
+#
+#     @series begin
+#         subplot := 4
+#         p3
+#     end
+# end
