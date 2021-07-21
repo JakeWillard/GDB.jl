197331d46ba11f9d71e26073efc6a11edf951e27
Jake WIllard <jacob.willard.gr@dartmouth.edu>
2021-07-21 14:03:15 -0400

    update simtools.jl
    can now save and load Setup structs.

--- src/prim/simtools.jl
+++ src/prim/simtools.jl
@@ -58,7 +58,10 @@ function load_sparse_matrix(fid, name)
     return sparse(Is, Js, Vs, n, m)
 end
 
-function save_setup(fid, stp::Setup)
+function save_setup(path, stp::Setup)
+
+    # open file
+    fid = h5open(path, "w")
 
     # save grid
     grd = stp.grid
@@ -73,18 +76,95 @@ function save_setup(fid, stp::Setup)
         save_sparse_matrix(fid, Restr, "Grid/Interp_$(i)")
     end
 
+    fid["Grid/nanmask"] = grd.nanmask[:,:]
     fid["Grid/points"] = grd.points[:,:]
     fid["Grid/origin"] = grd.origin[:]
-    fid["Grid/dims"] = Float64[grd.Nx, grd.Ny, grd.Nk, grd.mx, grd.my]
+    fid["Grid/dims"] = Int64[grd.Nx, grd.Ny, grd.Nk, grd.mx, grd.my]
     fid["Grid/spacing"] = Float64[grd.dx, grd.dy]
     fid["Grid/Mxinv"] = grd.Mxinv[:,:]
     fid["Grid/Myinv"] = grd.Myinv[:,:]
     fid["Grid/MxyinvT"] = grd.MxyinvT[:,:]
 
     # save operators
-    op = stp.operators
     create_group(fid, "Operators")
     for (name, M) in stp.operators
         save_sparse_matrix(fid, M, "Operators/$(name)")
     end
+
+    # close file
+    close(fid)
+end
+
+
+function Setup(path)
+
+    # open file
+    fid = h5open(path, "r")
+
+    # read grid
+    projection = load_sparse_matrix(fid, "Grid/Proj")
+    inverse_projection = load_sparse_matrix(fid, "Grid/ProjInv")
+    restrictions = SparseMatrixCSC[]
+    interpolations = SparseMatrixCSC[]
+
+    Nl = length(keys(fid["Grid"])) - 7
+    for i=1:Nl
+        append!(restrictions, [load_sparse_matrix(fid, "Grid/Restr_$(i)")])
+        append!(interpolations, [load_sparse_matrix(fid, "Grid/Interp_$(i)")])
+    end
+
+    nanmask = fid["Grid/nanmask"][:,:]
+    points = fid["Grid/points"][:,:]
+    origin = fid["Grid/origin"][:]
+    Nx, Ny, Nk, mx, my = fid["Grid/dims"][:]
+    dx, dy = fid["Grid/spacing"][:]
+    Mxinv = fid["Grid/Mxinv"][:,:]
+    Myinv = fid["Grid/Myinv"][:,:]
+    MxyinvT = fid["Grid/MxyinvT"][:,:]
+    grid = Grid(Grid(points, origin, projection, inverse_projection, restrictions, interpolations,
+                     nanmask, mx, my, Nx, Ny, Nk, Mxinv, Myinv, MxyinvT, dx, dy))
+
+
+    # read operators
+    operators = Dict{String, SparseMatrixCSC}
+    for name in keys(fid["Operators"])
+        operators[name] = load_sparse_matrix(fid, "Operators/$(name)")
+    end
+
+    # close file
+    close(fid)
+
+    return Setup(grid, operators)
+end
+
+
+function save_variables(path::String, vars...)
+
+    # open file
+    fid = h5open(path, "r+")
+
+    # read time index
+    t = fid["t"]
+
+    # save each var
+    for var in vars
+        fid["$(var.name)"][:,:,t] = var[:,:,2]
+    end
+
+    # increase time index
+    fid["t"] = t + 1
+
+    # close file
+    close(fid)
+
+end
+
+
+function integrate_then_save(path::String, tstep::Function, Nskp::Int64, params::Vector{Float64}, stp::Setup, vars...)
+
+    for _=1:Nskp
+        tstep(params, stp, vars...)
+    end
+
+    save_variables(path, vars...)
 end
