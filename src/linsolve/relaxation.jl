93b067a0d56812893a0d0b17212a0847587da184
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-07-31 20:25:22 -0400

    parallel solvers
    wrote parallel version of the linear solving functions.

--- src/linsolve/relaxation.jl
+++ src/linsolve/relaxation.jl
@@ -19,3 +19,21 @@ function jacobi_relaxation(N::Int64, A::SparseMatrixCSC, M::SparseMatrixCSC, D::
 
     return x0 + x
 end
+
+
+
+function asynch_jacobi_relaxation(N::Int64, M::SparseMatrixCSC, D::SparseMatrixCSC, x0::DArray, b::DArray)
+
+    DArray((size(x0)[1],), procs(x0), length(procs(x0))) do inds
+
+        Ms = M[inds[1],:]
+        Ds = D[inds[1],:]
+        xs = Array(x0)[:,1]
+        fs = Ds * Array(b)[:,1]
+        for _=1:N
+            xs[inds[1]] = Ms * xs + fs
+        end
+        xs[inds[1],1]
+    end
+
+end
--- src/linsolve/solvers.jl
+++ src/linsolve/solvers.jl
@@ -12,10 +12,7 @@ end
 
 
 
-function jacobi_preconditioned_gmres(L::LinearLeftHandSide, x0::Vector{Float64}, b::Vector{Float64}; n=100, m=10, err_thresh=1e-20)
-
-    # compute M and D
-    M, D = L.M, L.D
+function jacobi_preconditioned_gmres(L::LinearLeftHandSide, x0::Vector{Float64}, b::Vector{Float64}; n=100, m=10, err_thresh=1e-10)
 
     # precodition using jacobi_relaxation()
     x = jacobi_relaxation(n, L.A, L.M, L.D, x0, b)
@@ -24,4 +21,25 @@ function jacobi_preconditioned_gmres(L::LinearLeftHandSide, x0::Vector{Float64},
     x = gmres_solve(L.A, x, b, m; err_thresh=err_thresh)
 
     return x
+end
+
+
+function parallel_jacobi_preconditioned_gmres(L::LinearLeftHandSide, x0::Vector{Float64}, b::Vector{Float64}, pchunks::Int64, Nz::Int64; nsynch=100, nasynch=5, m=10, err_thresh=1e-10)
+
+    # jacobi preconditioning
+    x = distribute(x0; dist=pchunks*Nz)
+    bdist = distribute(b; dist=pchunks*Nz)
+    for _=1:nsynch
+        x = asynch_jacobi_relaxation(nasynch, L.M, L.D, x, bdist)
+    end
+
+    # do gmres in parallel
+    x = DArray((size(x)[1],), workers()[1:Nz], Nz) do inds
+
+        xs = Array(x0[inds[1],1])
+        bs = b[inds[1]]
+        gmres_solve(A, xs, bs, m, err_thresh=err_thresh)
+    end
+
+    return Array(x)
 end
