c1720bf99b2edc8734cb71c022ec29a53f046a95
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-01 11:36:02 -0400

    fixes
    Fixed typos and other minor bugs. Commented out calls to linsolve code until that is working properly.

--- experimental/calcs.jl
+++ experimental/calcs.jl
@@ -81,28 +81,31 @@ function A_forward_euler(A, A_t, Te, Ti, n, phi, ev, ad, de2, wrk::Workspace)
 
     Abohm = wrk.TRGT .* Abohm_def(Te, Ti, n, phi, ev, ad, de2)
 
-    return wrk.LAM*(A + wrk.dt*(wrk.P0*A_T + wrk.P3*Abohm))
+    return wrk.LAM*(A + wrk.dt*(wrk.P0*A_t + wrk.P3*Abohm))
 end
 
 
 function diffusion_lnn(lnn, wrk::Workspace)
 
     rhs = wrk.P0 * lnn
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnn, lnn, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_lnn.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnn, lnn, rhs, 1, wrk.GRID.Nz)
 end
 
 
 function diffusion_lnTe(lnTe, wrk::Workspace)
 
     rhs = wrk.P0 * lnTe
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnTe, lnTe, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_lnTe.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnTe, lnTe, rhs, 1, wrk.GRID.Nz)
 end
 
 
 function diffusion_lnTi(lnTi, wrk::Workspace)
 
     rhs = wrk.P0 * lnTi
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnTi, lnTi, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_lnTi.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_lnTi, lnTi, rhs, 1, wrk.GRID.Nz)
 end
 
 
@@ -110,14 +113,16 @@ function diffusion_u(u, Te, Ti, wrk::Workspace)
 
     cs = wrk.TRGT .* sqrt.(Te + Ti)
     rhs = wrk.P0 * u + wrk.DCHLT3*cs
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_u, u, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_u.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_u, u, rhs, 1, wrk.GRID.Nz)
 end
 
 
 function diffusion_w(w, wrk::Workspace)
 
     rhs = wrk.P0 * w
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_w, w, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_w.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_w, w, rhs, 1, wrk.GRID.Nz)
 end
 
 
@@ -125,28 +130,29 @@ function diffusion_A(A, Te, Ti, n, phi, ev, ad, de2, wrk::Workspace)
 
     Abohm = wrk.TRGT .* Abohm_def(Te, Ti, n, phi, ev, ad, de2)
     rhs = wrk.P0 * A + wrk.DCHLT3*Abohm
-    return parallel_jacobi_preconditioned_gmres(wrk.DIFF_A, A, rhs, 1, wrk.GRID.Nz)
+    return wrk.DIFF_A.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.DIFF_A, A, rhs, 1, wrk.GRID.Nz)
 end
 
 
 function vorticity_eqn(phi, w, Pi_xx, Pi_yy, Te, n, lnn_x, lnn_y, phi_b, ad, wrk::Workspace)
 
     M = Diagonal(n) * (wrk.Dxx + wrk.Dyy + Diagonal(lnn_x)*wrk.Dx + Diagonal(lnn_y)*wrk.Dy)
-    return M
     LHS = LinearLeftHandSide(wrk.P0*M + wrk.DCHLT1 + wrk.DCHLT2 + wrk.DCHLT3, 2/3)
 
     lambda = 2.695
     rhs0 = w - ad*(Pi_xx + Pi_yy)
     rhs = wrk.P0*rhs0 + wrk.DCHLT1*phi_b + lambda*(wrk.DCHLT2*Te + wrk.DCHLT3*Te)
-
-    return parallel_jacobi_preconditioned_gmres(LHS, phi, rhs, 1, wrk.GRID.Nz)
+    return LHS.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(LHS, phi, rhs, 1, wrk.GRID.Nz; m=100)
 end
 
 
 function helmholtz_eqn(psi, wrk::Workspace)
 
     rhs = wrk.P0 * psi
-    return parallel_jacobi_preconditioned(wrk.HHOLTZ, psi, rhs, 1, wrk.GRID.Nz)
+    return wrk.HHOLTZ.A \ rhs
+    # return parallel_jacobi_preconditioned_gmres(wrk.HHOLTZ, psi, rhs, 1, wrk.GRID.Nz)
 end
 
 
@@ -173,7 +179,7 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
         lnTe_xx = wrk.Dxx*lnTe[:,2]
         lnTe_yy = wrk.Dyy*lnTe[:,2]
         lnTe_s = partial_s(lnTe[:,2], lnTe_x, lnTe_y, psi_x, psi_y, wrk.Ds, am)
-        lnTe_ss = partial_ss(lnTe[:,2], lnTe_x, lnTe_y, lnTe_xy, lnTe_xx, lnTe_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
+        lnTe_ss = partial_ss(lnTe[:,2], lnTe_s, lnTe_x, lnTe_y, lnTe_xy, lnTe_xx, lnTe_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
 
         lnTe_t = lnT_partial_thermal(lnTe_s, lnTe_ss, Te, n, ke)
         lnTe[:,3] = wrk.LAM *(0.5*(lnTe[:,1] + lnTe[:,2]) + wrk.dt*(wrk.P0*lnTe_t + wrk.P1*wrk.STe)/wrk.N_subcycle)
@@ -185,7 +191,7 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
         lnTe_xx = wrk.Dxx*lnTe[:,2]
         lnTe_yy = wrk.Dyy*lnTe[:,2]
         lnTe_s = partial_s(lnTe[:,2], lnTe_x, lnTe_y, psi_x, psi_y, wrk.Ds, am)
-        lnTe_ss = partial_ss(lnTe[:,2], lnTe_x, lnTe_y, lnTe_xy, lnTe_xx, lnTe_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
+        lnTe_ss = partial_ss(lnTe[:,2], lnTe_s, lnTe_x, lnTe_y, lnTe_xy, lnTe_xx, lnTe_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
 
         lnTe_t = lnT_partial_thermal(lnTe_s, lnTe_ss, Te, n, ke)
         lnTe[:,3] = wrk.LAM *(lnTe[:,2] + wrk.dt*(wrk.P0*lnTe_t + wrk.P1*wrk.STe)/wrk.N_subcycle)
@@ -206,7 +212,7 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
         lnTi_xx = wrk.Dxx*lnTi[:,2]
         lnTi_yy = wrk.Dyy*lnTi[:,2]
         lnTi_s = partial_s(lnTi[:,2], lnTi_x, lnTi_y, psi_x, psi_y, wrk.Ds, am)
-        lnTi_ss = partial_ss(lnTi[:,2], lnTi_x, lnTi_y, lnTi_xy, lnTi_xx, lnTi_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
+        lnTi_ss = partial_ss(lnTi[:,2], lnTi_s, lnTi_x, lnTi_y, lnTi_xy, lnTi_xx, lnTi_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
 
         lnTi_t = lnT_partial_thermal(lnTi_s, lnTi_ss, Te, n, ke)
         lnTi[:,3] = wrk.LAM *(0.5*(lnTi[:,1] + lnTi[:,2]) + wrk.dt*(wrk.P0*lnTi_t + wrk.P1*wrk.STe)/wrk.N_subcycle)
@@ -218,7 +224,7 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
         lnTi_xx = wrk.Dxx*lnTi[:,2]
         lnTi_yy = wrk.Dyy*lnTi[:,2]
         lnTi_s = partial_s(lnTi[:,2], lnTi_x, lnTi_y, psi_x, psi_y, wrk.Ds, am)
-        lnTi_ss = partial_ss(lnTi[:,2], lnTi_x, lnTi_y, lnTi_xy, lnTi_xx, lnTi_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
+        lnTi_ss = partial_ss(lnTi[:,2], lnTi_s, lnTi_x, lnTi_y, lnTi_xy, lnTi_xx, lnTi_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, wrk.Dss, wrk.Ds, wrk.Dx, wrk.Dy, am)
 
         lnTi_t = lnT_partial_thermal(lnTi_s, lnTi_ss, Te, n, ke)
         lnTi[:,3] = wrk.LAM *(lnTi[:,2] + wrk.dt*(wrk.P0*lnTi_t + wrk.P1*wrk.STe)/wrk.N_subcycle)
@@ -292,23 +298,24 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
     G_x = wrk.Dx*G
     G_y = wrk.Dy*G
     G_c = -2*G_y[:]
+    G_s = partial_s(G, G_x, G_y, psi_x, psi_y, wrk.Ds, am)
 
     # evaluate total derivatives
-    lnn_Dt = lnn_total_ion_derivative(phi_c, Pe_c, n[:,2], j_s, u_s, er, ev, ad)
-    lnTe_Dt = lnTe_total_electron_derivative_adiabatic(lnn_Dt, j, lnn_s, n[:,2], Te, lnTe_c, er, ad)
+    lnn_Dt = lnn_total_ion_derivative(phi_c, Pe_c, n, j_s, u_s, er, ev, ad)
+    lnTe_Dt = lnTe_total_electron_derivative_adiabatic(lnn_Dt, j, lnn_s, n, Te, lnTe_c, er, ad)
     lnTi_Dt = lnTi_total_ion_derivative_adiabatic(lnn_Dt, Ti, lnTi_c, er, ad)
-    u_Dt = u_total_ion_derivative(Pe_s, Pi_s, G_s, n[:,2], Ti, u_c, ev, eg, er, ad)
+    u_Dt = u_total_ion_derivative(Pe_s, Pi_s, G_s, n, Ti, u_c, ev, eg, er, ad)
 
     # evaluate partial derivatives
     lnn_t = general_partial_derivative(lnn_Dt, lnn_x, lnn_y, lnn_s, phi_x, phi_y, u[:,2])
     lnTe_t = general_partial_derivative(lnTe_Dt, lnTe_x, lnTe_y, lnTe_s, phi_x, phi_y, u[:,2] - jn / ev)
     lnTi_t = general_partial_derivative(lnTi_Dt, lnTi_x, lnTi_y, lnTi_s, phi_x, phi_y, u[:,2])
     u_t = general_partial_derivative(u_Dt, u_x, u_y, u_s, phi_x, phi_y, u[:,2])
-    w_t = w_partial_t(n[:,2], lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
+    w_t = w_partial_t(n, lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
                          Pe_x, Pe_y, Pe_xy, Pe_xx, Pe_yy, Pe_xxx, Pe_yyy, Pe_xxy, Pe_xyy, Pe_c,
                          phi_x, phi_y, phi_xy, phi_xx, phi_yy, phi_xxx, phi_yyy, phi_xxy, phi_xyy,
                          G_c, Pi_c, j_s, ad, eg)
-    A_t = A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, u[:,2], n[:,2], Te, de2, ev, ad, am, eta)
+    A_t = A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, Pe_s, u[:,2], n, Te, de2, ev, ad, am, eta)
 
     # compute predictor step
     lnn[:,3] = lnn_forward_euler(0.5*(lnn[:,1] + lnn[:,2]), lnn_t, wrk)
@@ -316,16 +323,16 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
     lnTi[:,3] = lnTi_forward_euler(0.5*(lnTi[:,1] + lnTi[:,2]), lnTi_t, wrk)
     u[:,3] = u_forward_euler(0.5*(u[:,1] + u[:,2]), u_t, Te, Ti, wrk)
     w[:,3] = w_forward_euler(0.5*(w[:,1] + w[:,2]), w_t, wrk)
-    A[:,3] = A_forward_euler(0.5*(A[:,1] + A[:,2]), A_t, Te, Ti, n[:,2], phi, ev, ad, de2, wrk)
+    A[:,3] = A_forward_euler(0.5*(A[:,1] + A[:,2]), A_t, Te, Ti, n, phi, ev, ad, de2, wrk)
 
     # compute inner boundary value for phi
-    phi_b = dot(wrk.FLXAVG, phi)
+    phi_b = dot(wrk.FLXAVG, phi) * ones(wrk.GRID.Nk*wrk.GRID.Nz)
 
     # solve linear problems
     lnn[:,3] = diffusion_lnn(lnn[:,3], wrk)
     lnTe[:,3] = diffusion_lnTe(lnTe[:,3], wrk)
     lnTi[:,3] = diffusion_lnTi(lnTi[:,3], wrk)
-    u[:,3] = diffusion_u(u[:,3])
+    u[:,3] = diffusion_u(u[:,3], Te, Ti, wrk)
     w[:,3] = diffusion_w(w[:,3], wrk)
     phi = vorticity_eqn(phi, w[:,3], Pi_xx, Pi_yy, Te, n, lnn_x, lnn_y, phi_b, ad, wrk)
     psi = helmholtz_eqn(psi, wrk)
@@ -408,23 +415,24 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
     G_x = wrk.Dx*G
     G_y = wrk.Dy*G
     G_c = -2*G_y[:]
+    G_s = partial_s(G, G_x, G_y, psi_x, psi_y, wrk.Ds, am)
 
     # evaluate total derivatives
-    lnn_Dt = lnn_total_ion_derivative(phi_c, Pe_c, n[:,3], j_s, u_s, er, ev, ad)
-    lnTe_Dt = lnTe_total_electron_derivative_adiabatic(lnn_Dt, j, lnn_s, n[:,3], Te, lnTe_c, er, ad)
+    lnn_Dt = lnn_total_ion_derivative(phi_c, Pe_c, n, j_s, u_s, er, ev, ad)
+    lnTe_Dt = lnTe_total_electron_derivative_adiabatic(lnn_Dt, j, lnn_s, n, Te, lnTe_c, er, ad)
     lnTi_Dt = lnTi_total_ion_derivative_adiabatic(lnn_Dt, Ti, lnTi_c, er, ad)
-    u_Dt = u_total_ion_derivative(Pe_s, Pi_s, G_s, n[:,3], Ti, u_c, ev, eg, er, ad)
+    u_Dt = u_total_ion_derivative(Pe_s, Pi_s, G_s, n, Ti, u_c, ev, eg, er, ad)
 
     # evaluate partial derivatives
     lnn_t = general_partial_derivative(lnn_Dt, lnn_x, lnn_y, lnn_s, phi_x, phi_y, u[:,3])
     lnTe_t = general_partial_derivative(lnTe_Dt, lnTe_x, lnTe_y, lnTe_s, phi_x, phi_y, u[:,3] - jn / ev)
     lnTi_t = general_partial_derivative(lnTi_Dt, lnTi_x, lnTi_y, lnTi_s, phi_x, phi_y, u[:,3])
     u_t = general_partial_derivative(u_Dt, u_x, u_y, u_s, phi_x, phi_y, u[:,3])
-    w_t = w_partial_t(n[:,3], lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
+    w_t = w_partial_t(n, lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
                          Pe_x, Pe_y, Pe_xy, Pe_xx, Pe_yy, Pe_xxx, Pe_yyy, Pe_xxy, Pe_xyy, Pe_c,
                          phi_x, phi_y, phi_xy, phi_xx, phi_yy, phi_xxx, phi_yyy, phi_xxy, phi_xyy,
                          G_c, Pi_c, j_s, ad, eg)
-    A_t = A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, u[:,3], n[:,3], Te, de2, ev, ad, am, eta)
+    A_t = A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, Pe_s, u[:,3], n, Te, de2, ev, ad, am, eta)
 
     # compute corrector step
     lnn[:,3] = lnn_forward_euler(lnn[:,2], lnn_t, wrk)
@@ -435,15 +443,15 @@ function leapfrog!(lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn,
     A[:,3] = A_forward_euler(A[:,2], A_t, Te, Ti, n, phi, ev, ad, de2, wrk)
 
     # compute inner boundary value for phi
-    phi_b = dot(wrk.FLXAVG, phi)
+    phi_b = dot(wrk.FLXAVG, phi) * ones(wrk.GRID.Nk*wrk.GRID.Nz)
 
     # solve linear problems
     lnn[:,3] = diffusion_lnn(lnn[:,3], wrk)
     lnTe[:,3] = diffusion_lnTe(lnTe[:,3], wrk)
     lnTi[:,3] = diffusion_lnTi(lnTi[:,3], wrk)
-    u[:,3] = diffusion_u(u[:,3])
+    u[:,3] = diffusion_u(u[:,3], Te, Ti, wrk)
     w[:,3] = diffusion_w(w[:,3], wrk)
-    phi = vorticity_eqn(phi, w[:,3], Pi_xx, Pi_yy, Te, n[:,3], lnn_x, lnn_y, phi_b, ad, wrk)
+    phi = vorticity_eqn(phi, w[:,3], Pi_xx, Pi_yy, Te, n, lnn_x, lnn_y, phi_b, ad, wrk)
     psi = helmholtz_eqn(psi, wrk)
 
     # shift indices
--- src/physics/formulas.jl
+++ src/physics/formulas.jl
@@ -6,7 +6,7 @@ function partial_s(f, f_x, f_y, psi_x, psi_y, Ds, am)
 end
 
 
-function partial_ss(f, f_x, f_y, f_xy, f_xx, f_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, Dss, Ds, Dx, Dy, am)
+function partial_ss(f, f_s, f_x, f_y, f_xy, f_xx, f_yy, psi_x, psi_y, psi_xy, psi_xx, psi_yy, Dss, Ds, Dx, Dy, am)
 
     _a = Dss*f + am*Ds*(psi_x.*f_y - psi_y.*f_x) + am*(psi_x.*Dy*f_s - psi_y.*Dx*f_s)
     _b = (f_x.*psi_xy + f_xx.*psi_y - f_y.*psi_xx - psi_x.*f_xy).*psi_y
@@ -46,7 +46,7 @@ end
 function lnT_partial_thermal(lnT_s, lnT_ss, T, n, k)
 
     _a = (7/2)*lnT_s.^2 + lnT_ss
-    return k*T.^(5/2) * _a ./ n
+    return k*T.^(5/2) .* _a ./ n
 end
 
 
@@ -62,7 +62,7 @@ end
 
 function general_partial_derivative(f_Dt, f_x, f_y, f_s, phi_x, phi_y, v)
 
-    return f_Dt - phi_x.*f_y + phi_y.*f_x - v*f_s
+    return f_Dt - phi_x.*f_y + phi_y.*f_x - v.*f_s
 end
 
 
@@ -73,15 +73,15 @@ function w_partial_t(n, lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
                      G_c, Pi_c, j_s, ad, eg)
 
     _a = (lnn_x.*lnn_y + lnn_xy).*(Pe_y.*phi_y - Pe_x.*phi_x)
-    _b = (lnn_x.^2 + lnn_xx).*Pe_x.*phi_y - (lnn_y.^2 + lnn_yy)*Pe_y.*phi_x
+    _b = (lnn_x.^2 + lnn_xx).*Pe_x.*phi_y - (lnn_y.^2 + lnn_yy).*Pe_y.*phi_x
     _c = -Pe_x.*lnn_x.^2 .*phi_y + Pe_x.*lnn_x.*lnn_y.*phi_x
     _d = Pe_x.*lnn_x.*phi_xy - Pe_x.*lnn_y.*phi_xx + Pe_xx.*lnn_x.*phi_y
     _e = -Pe_xx.*lnn_y.*phi_x - Pe_xx.*phi_xy - Pe_xxx.*phi_y
     _f = -Pe_y.*lnn_x.*lnn_y.*phi_y + Pe_y.*lnn_x.*phi_yy
-    _g = Pe_y.*lnn_y.^2 .*phi_x - Pe_y.*lnn_y.^phi_xy + Pe_yy.*lnn_x.*phi_y
+    _g = Pe_y.*lnn_y.^2 .*phi_x - Pe_y.*lnn_y.*phi_xy + Pe_yy.*lnn_x.*phi_y
     _h = -Pe_yy.*lnn_y.*phi_x + Pe_yy.*phi_xy + Pe_yyy.*phi_x + phi_x.*Pe_xxy
     _i = phi_xx.*Pe_xy - ad.*phi_y.*Pe_xyy - phi_yy.*Pe_xy
-    _j = lnn_x.*phi_x*phi_xy - lnn_x.*phi_xx.*phi_y + lnn_y.*phi_x.*phi_yy - lnn_y.*phi_y.*phi_xy
+    _j = lnn_x.*phi_x.*phi_xy - lnn_x.*phi_xx.*phi_y + lnn_y.*phi_x.*phi_yy - lnn_y.*phi_y.*phi_xy
     _k = -lnn_x.*phi_xx.*phi_y + lnn_y.*phi_x.*phi_yy - lnn_y.*phi_y.*phi_xy
     _l = phi_x.*phi_yyy + phi_x.*phi_xxy - phi_xxx.*phi_y - phi_y.*phi_xyy
 
@@ -92,7 +92,7 @@ function w_partial_t(n, lnn_x, lnn_y, lnn_xy, lnn_xx, lnn_yy,
 end
 
 
-function A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, u, n, Te, de2, ev, ad, am, eta)
+function A_partial_t(phi_x, phi_y, phi_s, j, jn, jn_x, jn_y, jn_s, Pe_s, u, n, Te, de2, ev, ad, am, eta)
 
     _a = de2 * (phi_x .* jn_y - phi_y .* jn_x)
     _b = de2 * (u - jn / ev).*jn_s / ev
