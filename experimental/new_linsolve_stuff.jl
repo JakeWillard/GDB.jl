64f9ea216d280c012c5e0f31022fc46594301269
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-06 21:16:35 -0400

    Update new_linsolve_stuff.jl
    Fixed issue with extrapolating anti-symmetric boundary conditions.

--- experimental/new_linsolve_stuff.jl
+++ experimental/new_linsolve_stuff.jl
@@ -20,56 +20,134 @@ end
 
 
 
+
+
 struct GhostConditions
 
     Proj :: SparseMatrixCSC
     R :: SparseMatrixCSC
-    ghost_indices
+    ghost_indices :: Matrix{Int64}
 
 end
 
 
-function GhostConditions(mx, my, MinvT, parities::Vector{Int64}, bars::Vector{Barrier}, grd::Grid)
+function GhostConditions(mx, my, MinvT, bars::Vector{Barrier}, grd::Grid)
 
-    is = vcat([k*ones(Int64, mx*my) for k=1:grd.Nk]...)
-
-    js = Int64[]
-    dat = Float64[]
-    j_proj = Int64[]
-
-    ghost_indices = zeros(Int64, grd.Nk, length(bars))
-    ng = zeros(Int64, length(bars))
+    ks = [Int64[] for _=1:length(bars)+1]
+    js = [Int64[] for _=1:length(bars)+1]
+    dats = [Float64[] for _=1:length(bars)+1]
+    ng = 0
 
     for k=1:grd.Nk
         us = [smoothstep(grd.points[:,k]..., 1.0, bar) for bar in bars]
         if !isempty(us[us .< 0.5])
 
             us[us .> 0.5] .= -Inf
-            bind = sortperm(us)[end]
-            barr = bars[bind]
-            par = parities[bind]
+            l = sortperm(us)[end]
+            ng += 1
 
-            ng[bind] += 1
-            ghost_indices[ng[bind], bind] = k
+            # add permutation row
+            ks[l] = [ks[l]; k]
 
-            x, y = barr.rmap(grd.points[:,k]...)
+            # add reflection rows
+            x, y = bars[l].rmap(grd.points[:,k]...)
             row_dat, row_j = interpolation_row(x, y, mx, my, MinvT, grd)
-            js = [js; row_j]
-            dat = [dat; par*row_dat]
+            js[l] = [js[l]; row_j]
+            dats[l] = [dats[l]; row_dat]
+
+            # add identity rows
+            row_dat, row_j = interpolation_row(grd.points[:,k]..., mx, my, MinvT, grd)
+            js[l] = [js[l]; row_j]
+            dats[l] = [dats[l]; row_dat]
+
         else
 
-            row_dat, row_j = interpolation_row(grd.points[:,k]..., mx, my, MinvT, grd)
-            js = [js; row_j]
-            dat = [dat; row_dat]
-            j_proj = [j_proj; k]
+            # add permutation row
+            ks[end] = [ks[end]; k]
         end
     end
 
-    Proj = sparse(Int64[1:length(j_proj)...], j_proj, ones(length(j_proj)), length(j_proj), grd.Nk)
-    R = sparse(is, js, dat, grd.Nk, grd._Nx*grd._Ny) * transpose(grd.Proj)
+    # compute permutation and reflection condition matrix
+    Perm = sparse([1:grd.Nk...], vcat(ks...), ones(grd.Nk), grd.Nk, grd.Nk)
+    M = sparse(vcat([k*ones(2*mx*my) for k=1:ng]...), vcat(js...), vcat(dats...), ng, grd._Nx*grd._Ny) * transpose(grd.Proj) * transpose(Perm)
 
-    return GhostConditions(Proj, R, ghost_indices)
+    # invert to get extrapolation operation, delete small values
+    N = inv(Matrix(M[1:ng, 1:ng]))
+    N[abs.(N) .< 1e-8] .= 0
+    R = -sparse(N) * M[:, ng+1:end]
+
+    # make extrapolation matrix
+    Extr = sparse([1:grd.Nk]..., [1:grd.Nk]..., ones(Float64, grd.Nk), grd.Nk, grd.Nk)
+    Extr[1:ng, ng+1:end] = R
+    Extr[1:ng, 1:ng] .= 0
+
+    # undo permutation
+    return transpose(Perm) * Extr * Perm
 end
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+# function GhostConditions(mx, my, MinvT, parities::Vector{Int64}, bars::Vector{Barrier}, grd::Grid)
+#
+#     # is = vcat([k*ones(Int64, mx*my) for k=1:grd.Nk]...)
+#     is = Int64[]
+#     js = Int64[]
+#     dat = Float64[]
+#     j_proj = Int64[]
+#
+#     ghost_indices = zeros(Int64, grd.Nk, length(bars))
+#     ng = zeros(Int64, length(bars))
+#
+#     count = 0
+#     for k=1:grd.Nk
+#         us = [smoothstep(grd.points[:,k]..., 1.0, bar) for bar in bars]
+#         if !isempty(us[us .< 0.5])
+#
+#             us[us .> 0.5] .= -Inf
+#             bind = sortperm(us)[end]
+#             barr = bars[bind]
+#             par = parities[bind]
+#
+#             count += 1
+#             ng[bind] += 1
+#             ghost_indices[ng[bind], bind] = k
+#
+#             x, y = barr.rmap(grd.points[:,k]...)
+#             row_dat, row_j = interpolation_row(x, y, mx, my, MinvT, grd)
+#             is = [is; count*ones(Int64, mx*my)]
+#             js = [js; row_j]
+#             dat = [dat; par*row_dat]
+#
+#             row_dat, row_j = interpolation_row(grd.points[:,k]..., mx, my, MinvT, grd)
+#             is = [is; count*ones(Int64, mx*my)]
+#             js = [js; row_j]
+#             dat = [dat; row_dat]
+#         else
+#
+#             # row_dat, row_j = interpolation_row(grd.points[:,k]..., mx, my, MinvT, grd)
+#             # is = [is; k*ones(Int64, mx*my)]
+#             # js = [js; row_j]
+#             # dat = [dat; row_dat]
+#             j_proj = [j_proj; k]
+#         end
+#     end
+#
+#     Proj = sparse(Int64[1:length(j_proj)...], j_proj, ones(length(j_proj)), length(j_proj), grd.Nk)
+#     R = sparse(is, js, dat, grd.Nk - length(j_proj), grd._Nx*grd._Ny) * transpose(grd.Proj)
+#     return R
+#
+#     return GhostConditions(Proj, R, ghost_indices)
+# end
 
 
 function swap_parity(gc::GhostConditions, inds...)
