e0ef6b495d975a19d2d89af0fae3e01d7c2587d2
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-10 16:01:07 -0400

    Update example_simulation.jl
    example simulation now executes properly, but it diverges.

--- experimental/example_simulation.jl
+++ experimental/example_simulation.jl
@@ -1,5 +1,94 @@
 
 
+
+function save_sparse_matrix(fid, A::SparseMatrixCSC, name::String)
+
+    n, m = size(A)
+    Is, Js, Vs = findnz(A)
+
+    fid["$(name)_Is"] = Int32[Is...]
+    fid["$(name)_Js"] = Int32[Js...]
+    fid["$(name)_Vs"] = Float64[Vs...]
+    fid["$(name)_size"] = Int32[n, m]
+
+    @info "Sparse Matrix '$(name)' saved to disk."
+end
+
+
+function load_sparse_matrix(fid, name::String)
+
+    if isempty(fid["$(name)_Is"])
+        Is = Int32[1]
+        Js = Int32[1]
+        Vs = Float64[0]
+    else
+        Is = fid["$(name)_Is"][:]
+        Js = fid["$(name)_Js"][:]
+        Vs = fid["$(name)_Vs"][:]
+    end
+    n, m = fid["$(name)_size"][:]
+
+    @info "Loaded Sparse Matrix '$(name)'."
+
+    return sparse(Is, Js, Vs, n, m)
+end
+
+
+function save_grid(fid, grd::Grid, name::String)
+
+    # create group for grid
+    create_group(fid, name)
+
+    # save projection
+    save_sparse_matrix(fid, grd.Proj, "$(name)/Proj")
+    fid["$(name)/r0"] = grd.r0[:]
+    fid["$(name)/points"] = grd.points[:,:]
+    fid["$(name)/spacing"] = Float64[grd.dx, grd.dy]
+    fid["$(name)/_size"] = Int64[grd.Nk, grd.Nz, grd._Nx, grd._Ny, grd._Nbuffer]
+    fid["$(name)/_NaNs"] = grd._nan_outside_boundaries[:,:]
+
+    @info "Grid '$(name)' saved to disk."
+
+end
+
+
+function load_grid(fid, name::String)
+
+    Proj = load_sparse_matrix(fid, "$(name)/Proj")
+    r0 = fid["$(name)/r0"][:]
+    points = fid["$(name)/points"][:,:]
+    dx, dy = fid["$(name)/spacing"][:]
+    Nk, Nz, Nx, Ny, Nbuffer = fid["$(name)/_size"][:]
+    NaNs = fid["$(name)/_NaNs"][:,:]
+
+    @info "Loaded Grid '$(name)'."
+
+    return Grid(r0, points, Proj, dx, dy, Nk, Nz, Nx, Ny, Nbuffer, NaNs)
+end
+
+
+function save_ghost_conditions(fid, gc::GhostConditions, name::String)
+
+    # create group
+    create_group(fid, name)
+
+    save_sparse_matrix(fid, gc.Proj, "$(name)/Proj")
+    save_sparse_matrix(fid, gc.Extr, "$(name)/Extr")
+    fid["$(name)/swaps"] = gc.swaps[:,:]
+end
+
+
+function load_ghost_conditions(fid, name::String)
+
+    Proj = load_sparse_matrix(fid, "$(name)/Proj")
+    Extr = load_sparse_matrix(fid, "$(name)/Extr")
+    swaps = fid["$(name)/swaps"][:,:]
+
+    return GhostConditions(Proj, Extr, swaps)
+end
+
+
+
 function example_geometry_setup(path::String, Nx, Ny)
 
     # define the magnetic field
@@ -41,7 +130,7 @@ function example_geometry_setup(path::String, Nx, Ny)
     deltas = Float64[dp_in, 0.5*(dp_out + dp_priv), dr]
     barrs = [inner_flux, outer_flux, target]
     grd = Grid(Nx, Ny, 1; Nbuffer=0) do
-        inside(x,y) = check_if_inside(x, y, 2*deltas, barrs)
+        inside(x,y) = check_if_inside(x, y, 5*deltas, barrs)
         r0 = Float64[0.5, -1]
         r1 = Float64[1.5, 1]
         inside, r0, r1
@@ -56,7 +145,7 @@ function example_geometry_setup(path::String, Nx, Ny)
     end
 
     # K1 and K2 are operators for penalized forward-euler timesteps
-    K1 = Diagonal(pen[:,1] .* pen[:,2] .* pen[:,3])
+    K1 = sparse(Diagonal(pen[:,1] .* pen[:,2] .* pen[:,3]))
     K2 = 100 * (I - K1)
 
     # H1, H2, and H3 are for defining boundary values.
@@ -64,9 +153,9 @@ function example_geometry_setup(path::String, Nx, Ny)
     h1[pen[:,1] .!= 1.0] .= 0.0
     h2[pen[:,2] .!= 1.0] .= 0.0
     h3[pen[:,3] .!= 1.0] .= 0.0
-    H1 = Diagonal(h1)
-    H2 = Diagonal(h2)
-    H3 = Diagonal(h3)
+    H1 = sparse(Diagonal(h1))
+    H2 = sparse(Diagonal(h2))
+    H3 = sparse(Diagonal(h3))
 
     # compute ghost-conditions
     GC_dchlt = GhostConditions(2, 2, stencil2d(2, 2), barrs, grd)
@@ -77,7 +166,7 @@ function example_geometry_setup(path::String, Nx, Ny)
     trgt_sgn = f_to_grid((x,y) -> sign(1 - x), grd)
 
     # lcfs average
-    lcfs_avg = flux_surface_average(psi, 0.0, 150, 4, 4, stencil2d(4, 4), grd)
+    lcfs_avg = flux_surface_average(psi, 0.0, 150, Float64[1, 0], 4, 4, stencil2d(4, 4), grd)
 
     # compute derivatives (need to renormalize since solovev assumes R=1, and we need a=1 consistent with the normalization factors in publications.)
     Minv = stencil1d(5)
@@ -93,5 +182,165 @@ function example_geometry_setup(path::String, Nx, Ny)
     Dxyy = derivative_matrix(1, 2, Minv, Minv, grd) * (0.3)^3
     Ds, Dss = fieldline_derivatives(bx, by, bz, 0.01, 4, 4, MinvT, 1, grd)
 
-    return grd, GC_dchlt
+
+    fid = h5open(path, "w")
+
+    save_grid(fid, grd, "Grid")
+    save_sparse_matrix(fid, K1, "K1")
+    save_sparse_matrix(fid, K2, "K2")
+    save_sparse_matrix(fid, H1, "H1")
+    save_sparse_matrix(fid, H2, "H2")
+    save_sparse_matrix(fid, H3, "H3")
+    save_ghost_conditions(fid, GC_dchlt, "GC_dchlt")
+    save_ghost_conditions(fid, GC_nmann, "GC_nmann")
+    save_ghost_conditions(fid, GC_u, "GC_u")
+    fid["trgt_sgn"] = trgt_sgn[:]
+    save_sparse_matrix(fid, lcfs_avg, "lcfs_avg")
+    save_sparse_matrix(fid, Dx, "Dx")
+    save_sparse_matrix(fid, Dy, "Dy")
+    save_sparse_matrix(fid, Dxy, "Dxy")
+    save_sparse_matrix(fid, Dxx, "Dxx")
+    save_sparse_matrix(fid, Dyy, "Dyy")
+    save_sparse_matrix(fid, Dxxx, "Dxxx")
+    save_sparse_matrix(fid, Dyyy, "Dyyy")
+    save_sparse_matrix(fid, Dxxy, "Dxxy")
+    save_sparse_matrix(fid, Dxyy, "Dxyy")
+    save_sparse_matrix(fid, Ds, "Ds")
+    save_sparse_matrix(fid, Dss, "Dss")
+
+    close(fid)
+end
+
+
+function example_physics_setup(R0, n0, T0, B0, init_path, geo_path)
+
+    am, ad, ki, ke, er, eg, ev, de2, eta = dimensionless_parameters(0.3*R0, R0, n0, T0, B0)
+    psi, bx, by, bz = solovev_flux_function(0.1, 0.1, 0.3, 1.7, 10.0, downsep=[1, -0.561])
+
+    geofid = h5open(geo_path, "r")
+    grd = load_grid(geofid, "Grid")
+    Dx = load_sparse_matrix(geofid, "Dx")
+    Dy = load_sparse_matrix(geofid, "Dy")
+    Dxx = load_sparse_matrix(geofid, "Dxx")
+    Dyy = load_sparse_matrix(geofid, "Dyy")
+    GC_dchlt = load_ghost_conditions(geofid, "GC_dchlt")
+    lcfs_avg = load_sparse_matrix(geofid, "lcfs_avg")
+    H1 = load_sparse_matrix(geofid, "H1")
+    H2 = load_sparse_matrix(geofid, "H2")
+    H3 = load_sparse_matrix(geofid, "H3")
+    close(geofid)
+
+    psi_in = psi(1 - 0.28, 0)
+    dp = abs(psi_in)
+
+    n = Te = Ti = f_to_grid(grd) do x,y
+        psi_xy = psi(x,y)
+        (y > -0.561) ? (3/4)*(tanh((psi_in/2 - psi_xy)/dp)+1) + 0.5 : 0.5
+    end
+    uvec = f_to_grid(grd) do x,y
+        0.5*(tanh((-0.4 - y)/0.05) + 1)*tanh((1-x)/0.05)
+    end
+    u = hcat(uvec, uvec, uvec)
+    Sp = f_to_grid(grd) do x,y
+        psi_xy = psi(x,y)
+        (y > -0.561) ? 1/2 * (tanh((psi_in/2 - psi_xy)/dp) + 1) : 0.0
+    end
+
+    Pe = n .* Te
+    Pi = n .* Ti
+
+    lnn = lnTe = lnTi = begin v = log.(n); hcat(v, v, v) end
+    j = jn = psi = zeros(grd.Nk)
+    u = w = A = zeros(grd.Nk, 3)
+
+    lnn_x = Dx * lnn[:,2]
+    lnn_y = Dy * lnn[:,2]
+    Pi_xx = Dxx * Pi
+    Pi_yy = Dyy * Pi
+    phi_b = bval_phi(w[:,2], Te, lcfs_avg, H1, H2, H3)
+    phi = solve_vorticity_eqn(phi_b, w[:,2], n, lnn_x, lnn_y, Pi_xx, Pi_yy, ad, Dx, Dy, Dxx, Dyy, GC_dchlt)
+
+    fid = h5open(init_path, "w")
+    fid["lnn"] = lnn[:,:]
+    fid["lnTe"] = lnTe[:,:]
+    fid["lnTi"] = lnTi[:,:]
+    fid["u"] = u[:,:]
+    fid["w"] = w[:,:]
+    fid["A"] = A[:,:]
+    fid["phi"] = phi[:]
+    fid["psi"] = psi[:]
+    fid["n"] = n[:]
+    fid["Te"] = Te[:]
+    fid["Ti"] = Ti[:]
+    fid["Pe"] = Pe[:]
+    fid["Pi"] = Pi[:]
+    fid["j"] = j[:]
+    fid["jn"] = jn[:]
+    fid["Sp"] = Sp[:]
+    fid["params"] = Float64[am, ad, ki, ke, er, eg, ev, de2, eta]
+    close(fid)
+end
+
+
+function test_simulate(Nt, sn, ste, sti, kdiff, dt, output_path, init_path, geo_path)
+
+    geofid = h5open(geo_path, "r")
+    grd = load_grid(geofid, "Grid")
+    K1 = load_sparse_matrix(geofid, "K1")
+    K2 = load_sparse_matrix(geofid, "K2")
+    H1 = load_sparse_matrix(geofid, "H1")
+    H2 = load_sparse_matrix(geofid, "H2")
+    H3 = load_sparse_matrix(geofid, "H3")
+    GC_dchlt = load_ghost_conditions(geofid, "GC_dchlt")
+    GC_nmann = load_ghost_conditions(geofid, "GC_nmann")
+    GC_u = load_ghost_conditions(geofid, "GC_u")
+    trgt_sgn = geofid["trgt_sgn"][:]
+    lcfs_avg = load_sparse_matrix(geofid, "lcfs_avg")
+    Dx = load_sparse_matrix(geofid, "Dx")
+    Dy = load_sparse_matrix(geofid, "Dy")
+    Dxy = load_sparse_matrix(geofid, "Dxy")
+    Dxx = load_sparse_matrix(geofid, "Dxx")
+    Dyy = load_sparse_matrix(geofid, "Dyy")
+    Dxxx = load_sparse_matrix(geofid, "Dxxx")
+    Dyyy = load_sparse_matrix(geofid, "Dyyy")
+    Dxxy = load_sparse_matrix(geofid, "Dxxy")
+    Dxyy = load_sparse_matrix(geofid, "Dxyy")
+    Ds = load_sparse_matrix(geofid, "Ds")
+    Dss = load_sparse_matrix(geofid, "Dss")
+    close(geofid)
+
+    fid = h5open(init_path, "r")
+    lnn = fid["lnn"][:,:]
+    lnTe = fid["lnTe"][:,:]
+    lnTi = fid["lnTi"][:,:]
+    u = fid["u"][:,:]
+    w = fid["w"][:,:]
+    A = fid["A"][:,:]
+    phi = fid["phi"][:]
+    psi = fid["psi"][:]
+    n = fid["n"][:]
+    Te = fid["Te"][:]
+    Ti = fid["Ti"][:]
+    Pe = fid["Pe"][:]
+    Pi = fid["Pi"][:]
+    j = fid["j"][:]
+    jn = fid["jn"][:]
+    Sp = fid["Sp"][:]
+    am, ad, ki, ke, er, eg, ev, de2, eta = fid["params"][:]
+    close(fid)
+
+    Sn = sn * Sp
+    STe = ste * Sp
+    STi = sti * Sp
+    kdiff_lnn, kdiff_lnTe, kdiff_lnTi, kdiff_u, kdiff_w, kdiff_A = kdiff[:]
+    return phi, grd
+    for t=1:Nt
+        leapfrog!(1, lnn, lnTe, lnTi, u, w, A, phi, psi, n, Te, Ti, Pe, Pi, j, jn, Sn, STe, STi, Dx, Dy, Dxy, Dxx, Dyy, Dxxx, Dyyy,
+                  Dxxy, Dxyy, Ds, Dss, dt, 10, K1, K2, H1, H2, H3, trgt_sgn, lcfs_avg, GC_nmann, GC_dchlt, GC_u, kdiff_lnn,
+                  kdiff_lnTe, kdiff_lnTi, kdiff_u, kdiff_w, kdiff_A, am, ad, ki, ke, er, eg, ev, de2, eta)
+        # write to output
+        # ;alksdjfl;kasjfl;sk
+    end
+
+    return n, grd, GC_nmann
 end
