f603dc75065e525ade5405f247d4afc0b869f2f5
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-10 01:14:37 -0400

    Update maps.jl
    Uses line_average() to make averaging operators over closed fieldlines.

--- src/magflux/maps.jl
+++ src/magflux/maps.jl
@@ -56,3 +56,27 @@ function fieldline_derivatives(bx::Function, by::Function, bz::Function, ds::Flo
 
     return Ds, Dss
 end
+
+
+function flux_surface_average(psi::Function, psi_val, Ntheta, mx::Int64, my::Int64, MinvT::Matrix{Float64}, grd::Grid; dr=0.01)
+
+    points = zeros(2, Ntheta)
+    thetas = LinRange(0, 2*pi, Ntheta+1)
+    for i=1:Ntheta
+
+        et = Float64[cos(thetas[i]), sin(thetas[i])]
+        r = dr*et
+        sgn = sign(psi(r...) - psi_val)
+
+        while true
+            y = r + dr*et
+            if sgn*(psi(r...) - psi_val) < 0
+                points[:,i] = 0.5*(y + r)
+                break
+            end
+        end
+
+    end
+
+    return line_average(points, mx, my, MinvT, grd)
+end
