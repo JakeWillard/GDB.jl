057fc894fb449a61da972573a96878f43dd2b144
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-12 00:17:36 -0400

    Update new_linsolve_stuff.jl
    tidier jacobi smooth function that works in parallel.

--- experimental/new_linsolve_stuff.jl
+++ experimental/new_linsolve_stuff.jl
@@ -14,8 +14,36 @@ struct LinearMap
 
     Base.size(l::LinearMap) = length(l.f)
     Base.getindex(l::LinearMap, inds) = LinearMap(l.M[inds,:], l.f[inds])
-    Base.:*(l::LinearMap, v::Vector{Float64}) = l.M * v + f
+    Base.:*(l::LinearMap, v::Vector{Float64}) = l.M * v + l.f
     LinearMap(A::SparseMatrixCSC, b::Vector{Float64}) = new(A, b)
+end
+
+
+function test_jacobi(A::SparseMatrixCSC, x0, b, w, Nout, Nin, nchunks)
+
+    if nchunks > length(workers())
+        nchunks = 1
+    end
+
+    D = w*Diagonal(1 ./ diag(A))
+    J = LinearMap(I - D*A, Vector(D*b))
+    x = x0[:]
+
+    for _=1:Nout
+
+        xnext = DArray((length(x0),), workers()[1:nchunks], nchunks) do inds
+            Jp = J[inds[1]]
+            xp = x[:]
+            for __=1:Nin
+                xp[inds[1]] = Jp * xp
+            end
+            xp[inds[1]]
+        end
+        x[:] = Array(xnext)
+        @info err = norm(A*x - b)
+    end
+
+    return x
 end
 
 
