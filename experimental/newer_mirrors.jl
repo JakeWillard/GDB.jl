cef1236b908c7f8dd9670f4314b61f8dd114d09e
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-08-22 17:32:49 -0400

    Update newer_mirrors.jl
    more accurate mirror_image()

--- experimental/newer_mirrors.jl
+++ experimental/newer_mirrors.jl
@@ -2,7 +2,7 @@
 
 function distance_to_segment(x, y, vert0::Vector{Float64}, vert1::Vector{Float64})
 
-    xs, ys = Float64[x, y] - vert0
+    xs, ys = [x, y] - vert0
     t = begin dv = vert1 - vert0; atan(dv[2], dv[1]) end
 
     u = xs * cos(t) + ys * sin(t)
@@ -92,36 +92,80 @@ function distance_to_mirror(x, y, M::Mirror)
 end
 
 
-function mirror_image(x, y, dx, dy, M::Mirror)
+# function mirror_image(x, y, dx, dy, M::Mirror)
+#
+#     i = 0
+#     j = 0
+#     dist = 0
+#     steps = 0
+#
+#     while distance_to_mirror(x, y, M)[1] < 0
+#         hs = hcat([[distance_to_mirror(x+ii*dx, y+jj*dy, M)[1] for ii=-1:1] for jj=-1:1]...)
+#         di, dj = Tuple(findmax(hs)[2]) .- 2
+#         x += di*dx
+#         y += dj*dy
+#         i += di
+#         j += dj
+#         dist += sqrt(di^2 + dj^2)
+#         steps += di^2 + dj^2
+#     end
+#
+#     while (dist > 0) && (steps > 0)
+#         hs = hcat([[distance_to_mirror(x+ii*dx, y+jj*dy, M)[1] for ii=-1:1] for jj=-1:1]...)
+#         di, dj = Tuple(findmax(hs)[2]) .- 2
+#         x += di*dx
+#         y += dj*dy
+#         i += di
+#         j += dj
+#         dist -= sqrt(di^2 + dj^2)
+#         steps -= 1
+#     end
+#
+#     return x, y, i, j
+# end
 
-    i = 0
-    j = 0
+
+function mirror_image(x0, y0, dx, dy, M::Mirror)
+
+    H(r) = distance_to_mirror(r[1], r[2], M)[1]
+    gradH(r) = ForwardDiff.gradient(H, r)
+
+    x = x0
+    y = y0
+    i = 0.0
+    j = 0.0
     dist = 0
     steps = 0
 
-    while distance_to_mirror(x, y, M)[1] < 0
-        hs = hcat([[distance_to_mirror(x+ii*dx, y+jj*dy, M)[1] for ii=-1:1] for jj=-1:1]...)
-        di, dj = Tuple(findmax(hs)[2]) .- 2
-        x += di*dx
-        y += dj*dy
-        i += di
-        j += dj
-        dist += sqrt(di^2 + dj^2)
-        steps += di^2 + dj^2
+    while H([x,y]) < 0
+        v = gradH([x,y])
+        v = v / norm(v)
+        @assert !any(isinf.(v))
+        i += v[1]
+        j += v[2]
+        x = x0 + dx*round(i)
+        y = y0 + dy*round(j)
+        # step =
+        steps += 1
+        dist += norm(round.(v))
+        # dist += sqrt(dstep)
     end
 
+    steps = 2 * steps
     while (dist > 0) && (steps > 0)
-        hs = hcat([[distance_to_mirror(x+ii*dx, y+jj*dy, M)[1] for ii=-1:1] for jj=-1:1]...)
-        di, dj = Tuple(findmax(hs)[2]) .- 2
-        x += di*dx
-        y += dj*dy
-        i += di
-        j += dj
-        dist -= sqrt(di^2 + dj^2)
+        v = gradH([x,y])
+        v = v / norm(v)
+        i += v[1]
+        j += v[2]
+        x = x0 + dx*round(i)
+        y = y0 + dy*round(j)
+        # dstep = i^2 + j^2
         steps -= 1
+        dist -= norm(round.(v))
+        # dist -= sqrt(dstep)
     end
 
-    return x, y, i, j
+    return x, y, round(i), round(j)
 end
 
 
@@ -155,7 +199,7 @@ function GhostData(M::Mirror, grd::Grid)
         next!(p)
     end
 
-    map_out = pmap(1:grd.Nk) do k
+    map_out = map(1:grd.Nk) do k
 
         _, proj_js, _ = findnz(grd.Proj)
         k_cart = proj_js[k]
