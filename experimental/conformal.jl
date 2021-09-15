3eb9ffed5c6c0ec4a4d90d5e59dd6706a6ef795a
JakeWillard <54251585+JakeWillard@users.noreply.github.com>
2021-09-15 10:14:53 -0400

    Update conformal.jl
    implemented the conformal map I derived for the d-shaped geometry. It works!

--- experimental/conformal.jl
+++ experimental/conformal.jl
@@ -42,3 +42,40 @@ function visualize_map(f::Function, L, N)
     contour!(xs, ys, z_imag, levels=50) # levels=ys[1:skip:end], colorbar=false)
     return p
 end
+
+
+function map_polar(f::Function, r, N)
+
+    ts = LinRange(0, 2*pi, N)
+    zs = r*(cos.(ts) + im*sin.(ts))
+    ws = f.(zs)
+
+    xs = real.(ws)
+    ys = imag.(ws)
+    return xs, ys
+end
+
+
+function map_d_shape(kappa, delta, Nr, Nt)
+
+    alpha = asin(delta)
+    rs = LinRange(1.0, 2.0, Nr)
+    xs = zeros(Nt, Nr)
+    ys = zeros(Nt, Nr)
+    for i=1:Nr
+        xs[:,i], ys[:,i] = map_polar(rs[i], Nt) do z
+            elpart = (1 + kappa)*z/2.0 + (1 - kappa)/(2*z)
+            sumpart = 0
+            for j=1:5
+                n = 2*j
+                _a = (-1)^(n-2)*alpha^(n-1) / (factorial(n-1)*2^n)
+                _b = 1 + (alpha/(2*n))*(z + 1/z)
+                _c = (z - 1/z)^n
+                sumpart += _a * _b * _c
+            end
+            elpart + sumpart
+        end
+    end
+
+    return xs, ys
+end
