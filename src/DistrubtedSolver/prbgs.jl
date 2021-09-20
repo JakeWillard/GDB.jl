b8e1081389803fa0affdc734f2ad0e77c3f65c3f
Jake WIllard <jacob.willard.gr@dartmouth.edu>
2021-09-20 13:19:03 -0400

    DistributedSolver
    put parallel-random-block_gauss-seidel and parallel-gmres functions into their own module

---
+++ src/DistrubtedSolver/prbgs.jl
@@ -0,0 +1,36 @@
+
+# parallel-random-block-gauss-seidel
+function prbgs(A::SparseMatrixCSC, x0::Vector{Float64}, b::Vector{Float64}; block_fraction=0.2, Np=2)
+
+    Nk = length(x0)
+    chunksize = Int64(floor(block_fraction*Nk/Np))
+    offset = div(chunksize, 2)
+    slices = [collect(Iterators.partition(1:Nk, chunksize))...,
+              collect(Iterators.partition(offset:Nk, chunksize))...]
+    Ns = length(slices)
+
+    x = x0[:]
+    r = b - A*x
+    bnorm = norm(b)
+
+    err = norm(r) / bnorm
+    while err > 1e-10
+
+        picked = [slices[rand(1:end)] for _=1:Np]
+
+        deltas = pmap(picked) do slc
+            A[:,slc] \ r
+        end
+
+        for i=1:Np
+            slc = picked[i]
+            x[slc] = x[slc] + deltas[i]
+        end
+
+        r[:] = b - A*x
+        err = norm(r) / bnorm
+        @info err
+    end
+
+    return x
+end
