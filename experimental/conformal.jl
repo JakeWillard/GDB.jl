5d146e2c463626e13af3edf83c2f7862633a351f
Jake WIllard <jacob.willard.gr@dartmouth.edu>
2021-09-22 12:00:11 -0400

    update conformal.jl
    inverse experiment

--- experimental/conformal.jl
+++ experimental/conformal.jl
@@ -1,95 +1,44 @@
 
+function circle_to_shape(z, alpha, kappa)
 
-
-
-
-function shaped_transform(N, delta, kappa)
-
-    epsilon = 1.0
-    ts = LinRange(0, 2*pi, N+1)[1:N]
-    alpha = asin(delta)
-    x = epsilon * cos.(ts + alpha*sin.(ts))
-    y = epsilon * kappa * sin.(ts)
-    p = Polygon(x, y)
-    m = ExteriorMap(p)
-
-    f(x,y) = begin
-        w = m(x + y*im)
-        real(w), imag(w)
-    end
-
-    return f
+    _a = 0.5*(z + 1/z)
+    _b = 0.5*(z - 1/z)
+    return _a*cosh(alpha*_b) + _b*sinh(alpha*_b) + kappa*_b
 end
 
 
-function visualize_map(f::Function, L, N)
+function diff_circle_to_shape(z, alpha, kappa)
 
-    N = 100
-    xs = LinRange(-L/2, L/2, N)
-    ys = LinRange(-L/2, L/2, N)
-    z_real = zeros(N, N)
-    z_imag = zeros(N, N)
+    _a = cosh(alpha*(z - 1/z)/2)
+    _b = sinh(alpha*(z - 1/z)/2)
+    _c = _a + _b
+    _d = _a - _b
+    _e = (_c + kappa) - (_d - kappa)/z^2
+    _f = 0.5*alpha*(z + 1/z)*_c
+    _g = 0.5*alpha*(1/z + 1/z^3)*_d
 
-    for i=1:N
-        for j=1:N
-            w = f(xs[i] + ys[j]*im)
-            z_real[j, i] = real(w)
-            z_imag[j, i] = imag(w)
-        end
-    end
-
-    p = contour(xs, ys, z_real, levels=50) # levels=xs[1:skip:end], colorbar=false)
-    contour!(xs, ys, z_imag, levels=50) # levels=ys[1:skip:end], colorbar=false)
-    return p
+    return (_f + _g) / 2
 end
 
 
+function shape_to_circle(w, alpha, kappa)
 
+    r = circle_to_shape(w, alpha, kappa) - w
+    z = w
 
-# function d_shape_map(z, kappa, alpha)
-#
-#     out = (1 + kappa)*z/2.0 + (1 - kappa)/(2*z)
-#     for j=1:5
-#         n = 2*j
-#         _a = (-1)^(n-2)*alpha^(n-1) / (factorial(n-1)*2^n)
-#         _b = 1 + (alpha/(2*n))*(z + 1/z)
-#         _c = (z - 1/z)^n
-#         out += _a * _b * _c
-#     end
-#     out
-# end
-
-
-function d_shape_map(z, kappa, alpha)
-
-    _a = 0.5*(z + 1/z)*cosh(alpha*(z - 1/z)/2)
-    _b = 0.5*(z - 1/z)*sinh(alpha*(z - 1/z)/2)
-    _c = kappa*(z - 1/z)/2.0
-    return _a + _b + _c
-end
-
-
-
-
-function vis_edge_coords(kappa, delta, Nr, Nt)
-
-    alpha = asin(delta)
-    p = plot()
-
-    rs = LinRange(1.0, 1.5, Nr)
-    ts = LinRange(0, 2*pi, Nt)
-
-    for r in rs
-        zs = r*exp.(im * ts)
-        ws = [d_shape_map(z, kappa, alpha) for z in zs]
-        plot!(p, real.(ws), imag.(ws), color=:red, legend=false, aspect_ratio=:equal)
+    xs = zeros(10)
+    ys = zeros(10)
+    for i=1:10
+        z -= r / diff_circle_to_shape(z, alpha, kappa)
+        r = circle_to_shape(z, alpha, kappa) - w
+        xs[i] = real(z)
+        ys[i] = imag(z)
     end
+    # while abs(r) > 1e-8
+    #     z -= r / diff_circle_to_shape(z, alpha, kappa)
+    #     r = circle_to_shape(z, alpha, kappa) - w
+    # end
 
-    for t in ts
-        zs = rs * exp(im * t)
-        ws = [d_shape_map(z, kappa, alpha) for z in zs]
-        plot!(p, real.(ws), imag.(ws), color=:blue, legend=false, aspect_ratio=:equal)
-    end
-
+    p = plot(xs, ys)
     return p
 end
